c4340a4d4768ed2f0906c185e925a1f65454fb34
高高子健 <gaogaozijian@gaogaozijiandeMac-mini.local>
2025-01-18 00:16:06 +0800

    1

--- ios/SudokuApp/AppDelegate.mm
+++ ios/SudokuApp/AppDelegate.mm
@@ -1,6 +1,8 @@
 #import "AppDelegate.h"
 
+#import <React/RCTBridge.h>
 #import <React/RCTBundleURLProvider.h>
+#import <React/RCTRootView.h>
 #import <GoogleMobileAds/GoogleMobileAds.h>
 
 @implementation AppDelegate
@@ -9,20 +11,27 @@
 {
   [[GADMobileAds sharedInstance] startWithCompletionHandler:nil];
   
-  self.moduleName = @"SudokuApp";
-  // You can add your custom initial props in the dictionary below.
-  // They will be passed down to the ViewController used by React Native.
-  self.initialProps = @{};
+  RCTBridge *bridge = [[RCTBridge alloc] initWithDelegate:self launchOptions:launchOptions];
+  RCTRootView *rootView = [[RCTRootView alloc] initWithBridge:bridge
+                                                   moduleName:@"SudokuApp"
+                                            initialProperties:nil];
 
-  return [super application:application didFinishLaunchingWithOptions:launchOptions];
+  if (@available(iOS 13.0, *)) {
+    rootView.backgroundColor = [UIColor systemBackgroundColor];
+  } else {
+    rootView.backgroundColor = [UIColor whiteColor];
+  }
+
+  self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
+  UIViewController *rootViewController = [UIViewController new];
+  rootViewController.view = rootView;
+  self.window.rootViewController = rootViewController;
+  [self.window makeKeyAndVisible];
+  
+  return YES;
 }
 
 - (NSURL *)sourceURLForBridge:(RCTBridge *)bridge
-{
-  return [self bundleURL];
-}
-
-- (NSURL *)bundleURL
 {
 #if DEBUG
   return [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index"];
