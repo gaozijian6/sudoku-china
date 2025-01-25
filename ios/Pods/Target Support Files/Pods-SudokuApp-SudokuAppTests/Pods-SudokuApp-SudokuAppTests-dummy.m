11fb585cdbc8736a24ff47bc4048f3765195f5c7
高高子健 <gaogaozijian@gaogaozijiandeMac-mini.local>
2025-01-25 13:12:00 +0800

    6

---
+++ ios/Pods/Target Support Files/Pods-SudokuApp-SudokuAppTests/Pods-SudokuApp-SudokuAppTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SudokuApp_SudokuAppTests : NSObject
+@end
+@implementation PodsDummy_Pods_SudokuApp_SudokuAppTests
+@end
---
+++ ios/Pods/Target Support Files/ReactCodegen/ReactCodegen-umbrella.h
@@ -0,0 +1,63 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+#import "FBReactNativeSpec/FBReactNativeSpec.h"
+#import "FBReactNativeSpecJSI.h"
+#import "RCTModulesConformingToProtocolsProvider.h"
+#import "react/renderer/components/rngesturehandler_codegen/ComponentDescriptors.h"
+#import "react/renderer/components/rngesturehandler_codegen/EventEmitters.h"
+#import "react/renderer/components/rngesturehandler_codegen/Props.h"
+#import "react/renderer/components/rngesturehandler_codegen/RCTComponentViewHelpers.h"
+#import "react/renderer/components/rngesturehandler_codegen/ShadowNodes.h"
+#import "react/renderer/components/rngesturehandler_codegen/States.h"
+#import "react/renderer/components/RNGoogleMobileAdsSpec/ComponentDescriptors.h"
+#import "react/renderer/components/RNGoogleMobileAdsSpec/EventEmitters.h"
+#import "react/renderer/components/RNGoogleMobileAdsSpec/Props.h"
+#import "react/renderer/components/RNGoogleMobileAdsSpec/RCTComponentViewHelpers.h"
+#import "react/renderer/components/RNGoogleMobileAdsSpec/ShadowNodes.h"
+#import "react/renderer/components/RNGoogleMobileAdsSpec/States.h"
+#import "react/renderer/components/RNGoogleSignInCGen/ComponentDescriptors.h"
+#import "react/renderer/components/RNGoogleSignInCGen/EventEmitters.h"
+#import "react/renderer/components/RNGoogleSignInCGen/Props.h"
+#import "react/renderer/components/RNGoogleSignInCGen/RCTComponentViewHelpers.h"
+#import "react/renderer/components/RNGoogleSignInCGen/ShadowNodes.h"
+#import "react/renderer/components/RNGoogleSignInCGen/States.h"
+#import "react/renderer/components/rnsvg/ComponentDescriptors.h"
+#import "react/renderer/components/rnsvg/EventEmitters.h"
+#import "react/renderer/components/rnsvg/Props.h"
+#import "react/renderer/components/rnsvg/RCTComponentViewHelpers.h"
+#import "react/renderer/components/rnsvg/ShadowNodes.h"
+#import "react/renderer/components/rnsvg/States.h"
+#import "react/renderer/components/safeareacontext/ComponentDescriptors.h"
+#import "react/renderer/components/safeareacontext/EventEmitters.h"
+#import "react/renderer/components/safeareacontext/Props.h"
+#import "react/renderer/components/safeareacontext/RCTComponentViewHelpers.h"
+#import "react/renderer/components/safeareacontext/ShadowNodes.h"
+#import "react/renderer/components/safeareacontext/States.h"
+#import "rnasyncstorage/rnasyncstorage.h"
+#import "rnasyncstorageJSI.h"
+#import "rngesturehandler_codegen/rngesturehandler_codegen.h"
+#import "rngesturehandler_codegenJSI.h"
+#import "RNGoogleMobileAdsSpec/RNGoogleMobileAdsSpec.h"
+#import "RNGoogleMobileAdsSpecJSI.h"
+#import "RNGoogleSignInCGen/RNGoogleSignInCGen.h"
+#import "RNGoogleSignInCGenJSI.h"
+#import "RNImagePickerSpec/RNImagePickerSpec.h"
+#import "RNImagePickerSpecJSI.h"
+#import "rnsvg/rnsvg.h"
+#import "rnsvgJSI.h"
+#import "safeareacontext/safeareacontext.h"
+#import "safeareacontextJSI.h"
+
+FOUNDATION_EXPORT double ReactCodegenVersionNumber;
+FOUNDATION_EXPORT const unsigned char ReactCodegenVersionString[];
+
